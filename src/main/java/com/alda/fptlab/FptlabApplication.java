e611f1166450ae9a009b0f5c03bf0c00695bdc94
Dario Cascone <casconedario93@gmail.com>
2022-06-20 01:03:49 +0200

    initial commit

---
+++ src/main/java/com/alda/fptlab/FptlabApplication.java
@@ -0,0 +1,13 @@
+package com.alda.fptlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FptlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FptlabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alda/fptlab/FptlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alda.fptlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FptlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
