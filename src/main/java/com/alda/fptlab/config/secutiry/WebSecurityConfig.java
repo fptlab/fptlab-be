c7aa229a91eb29462fe0fcc4c1003ae43e18ddf1
AlessandroColonna <alessandrocolonna10@gmail.com>
2022-07-25 16:36:10 +0200

    - CORS

--- src/main/java/com/alda/fptlab/config/secutiry/WebSecurityConfig.java
+++ src/main/java/com/alda/fptlab/config/secutiry/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.alda.fptlab.config.secutiry;
 
-import com.alda.fptlab.filter.CORSFilter;
 import com.alda.fptlab.filter.CustomAuthenticationFilter;
 import com.alda.fptlab.filter.CustomAuthorizationFilter;
 import com.alda.fptlab.service.impl.UserDetailsServiceImpl;
@@ -16,6 +15,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -38,6 +42,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource()
+    {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
@@ -51,7 +66,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeHttpRequests().antMatchers(WHITE_LIST_URLS).permitAll();
         http.authorizeHttpRequests().anyRequest().authenticated();
         http.addFilter(new CustomAuthenticationFilter(authenticationManager()));
-        http.addFilter(new CORSFilter());
         http.addFilterBefore(new CustomAuthorizationFilter(authenticationManager()), UsernamePasswordAuthenticationFilter.class);
     }
 }
