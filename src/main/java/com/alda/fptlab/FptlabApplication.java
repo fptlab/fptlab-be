a75eef7bd8ea10d4298fc2161d9f8b77dbf88c85
AlessandroColonna <alessandrocolonna10@gmail.com>
2023-01-05 17:26:09 +0100

    - google cloud

--- src/main/java/com/alda/fptlab/FptlabApplication.java
+++ src/main/java/com/alda/fptlab/FptlabApplication.java
@@ -2,8 +2,6 @@ package com.alda.fptlab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,4 +12,5 @@ public class FptlabApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FptlabApplication.class, args);
 	}
+
 }
